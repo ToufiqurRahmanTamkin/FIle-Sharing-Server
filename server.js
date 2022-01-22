bcf595267f530b88f3e58ce77e232ac452c79817
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2022-01-23 01:37:06 +0600

    file upload api added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db")
 connectDB();
 
+// routes
+app.use('/api/files', require('./routes/files'));
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
