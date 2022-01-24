cf8d7632bac8653776200d8960a0f8b131a5d979
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2022-01-24 10:59:13 +0600

    ejs template setup for download page

--- routes/show.js
+++ routes/show.js
@@ -5,17 +5,15 @@ router.get("/:uuid", async (req, res) => {
   try {
     const file = await File.findOne({ uuid: req.params.uuid });
     if (!file) {
-        return res.render("download", { error: "link has been expired" });
+      return res.render("download", { error: "link has been expired" });
     }
 
-    return res.render('download',{
-        uuid: file.uuid,
-        fileName: file.fileName,
-        fileSize: file.size,
-        download: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-
-    })
-
+    return res.render("download", {
+      uuid: file.uuid,
+      fileName: file.filename,
+      fileSize: file.size,
+      downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`,
+    });
   } catch (err) {
     return res.render("download", { error: "something went wrong" });
   }
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const app = express();
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
-
-const connectDB = require("./config/db")
+app.use(express.static("public"));
+const connectDB = require("./config/db");
 connectDB();
 
 // template engine
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 // routes
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'))
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
