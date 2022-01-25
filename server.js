fb20bd817c31b44c0a71ddc3f6c7708e3ad7a342
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2022-01-25 11:58:31 +0600

    download file api implemented

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.set("view engine", "ejs");
 // routes
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
+app.use('/files/download', require("./routes/download"));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
